56f757758678d5dda7dea880a23ff4b774427f84
frenchj <jason.french@wsj.com>
2016-10-31 10:59:42 +0000

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+a fun litle redux-y voting client
+
+`npm install webpack-dev-server && webpack-dev-server`
---
+++ src/index.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import io from 'socket.io-client';
+import reducer from './reducer';
+import {setState} from './action_creators';
+import remoteActionMiddleware from './remote_action_middleware';
+import App from './components/App';
+import {VotingContainer} from './components/Voting';
+import {ResultsContainer} from './components/Results';
+
+require('./css/style.css');
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state =>
+  store.dispatch(setState(state))
+);
+
+const createStoreWithMiddleware = applyMiddleware(
+  remoteActionMiddleware(socket)
+)(createStore);
+const store = createStoreWithMiddleware(reducer);
+
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ["Homer at the Bat", "Treehouse of Horror V"],
+      tally: {
+        "Homer at the Bat": 2
+      }
+    }
+  }
+});
+
+const routes = <Route component={App}>
+  <Route path="/results" component={ResultsContainer} />
+  <Route path="/" component={VotingContainer} />
+</Route>
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={hashHistory}>{routes}</Router>
+  </Provider>,
+  document.getElementById('app')
+);
